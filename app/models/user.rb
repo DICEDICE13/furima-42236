32c7e245236b2c7e7f315796d5e8510bc0dcc296
DICEDICE13 <orca.works2022@gmail.com>
2025-08-01 15:21:12 +0900

    指摘コード修正②

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,20 +7,20 @@ class User < ApplicationRecord
   validates :nickname, presence: true
 
   # パスワードは半角英数字混合かどうか
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/
+  VALID_PASSWORD_REGEX = /\A(?=.*[a-z])(?=.*\d)[a-z\d]+\z/i
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数字混合で入力してください' }
 
   # 名前（全角）バリデーション
   validates :last_name,  presence: true,
-                         format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+                         format: { with: /\A[ぁ-んァ-ン一-龥々ーヶ]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
   validates :first_name, presence: true,
-                         format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+                         format: { with: /\A[ぁ-んァ-ン一-龥々ーヶ]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
 
   # 名前カナ（全角カタカナ）バリデーション
   validates :last_name_kana,  presence: true,
-                              format: { with: /\A[ァ-ンー－]+\z/, message: 'は全角カタカナで入力してください' }
+                              format: { with: /\A[ァ-ンーヶ]+\z/, message: 'は全角カタカナで入力してください' }
   validates :first_name_kana, presence: true,
-                              format: { with: /\A[ァ-ンー－]+\z/, message: 'は全角カタカナで入力してください' }
+                              format: { with: /\A[ァ-ンーヶ]+\z/, message: 'は全角カタカナで入力してください' }
 
   
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,59 @@ RSpec.describe User, type: :model do
     end
 
     context '登録できないとき' do
+      it 'nicknameが空では登録できない' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+
+      it 'emailが空では登録できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+
+      it '重複したemailは登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+
+      it 'emailに@が含まれていないと登録できない' do
+        @user.email = 'testmail.com'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
+
+      it 'passwordが空では登録できない' do
+        @user.password = ''
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+
+      it 'passwordが6文字未満では登録できない' do
+        @user.password = 'a1b2c'
+        @user.password_confirmation = 'a1b2c'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+
+      it 'passwordに全角文字が含まれていると登録できない' do
+        @user.password = 'ａｂｃ１２３'
+        @user.password_confirmation = 'ａｂｃ１２３'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password は半角英数字混合で入力してください')
+      end
+
+      it 'password_confirmationがpasswordと一致しないと登録できない' do
+        @user.password = 'abc123'
+        @user.password_confirmation = 'abc124'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+
       it '生年月日が空では登録できない' do
         @user.birthday = ''
         @user.valid?
