b395f8b31de34c85e45ddc88b7d07ba6e56aef82
DICEDICE13 <orca.works2022@gmail.com>
2025-08-07 23:04:15 +0900

    モデルの単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
 
   validates :image, :name, :description, :category_id, :status_id, :shipping_fee_status_id, :prefecture_id, :delivery_day_id, :price, presence: true
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-  validates :category_id, :status_id, :shipping_fee_status_id, :delivery_day_id, :prefecture_id, numericality: { other_than: 1, message: "を選択してください" }
+  validates :category_id, :status_id, :shipping_fee_status_id, :delivery_day_id, :prefecture_id, numericality: { other_than: 1, message: "must be other than 1" }
   validates :name, presence: true
   validates :description, presence: true
 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item) # FactoryBot を使う想定
+  end
+
+  describe '商品出品機能' do
+    context '出品できるとき' do
+      it 'すべての項目が適切に入力されていれば出品できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '出品できないとき' do
+      it 'nameが空では出品できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it 'descriptionが空では出品できない' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+
+      it 'priceが空では出品できない' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it 'priceが300未満では出品できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+
+      it 'priceが10000000以上では出品できない' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+
+      it 'priceが全角数字では出品できない' do
+        @item.price = '５００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+      it 'category_idが1（---）では出品できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+    end
+  end
 end
+
