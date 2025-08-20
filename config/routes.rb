c8dc3d9d586a9941cc2d924223126db6e1b89433
DICEDICE13 <orca.works2022@gmail.com>
2025-08-20 15:53:48 +0900

    編集ボタンを押して編集画面に遷移するように変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   root to: "items#index"
   devise_for :users
 
-  resources :items, only: [:index, :new, :create, :show, :edit]
-  
+  resources :items
   
   
 end
