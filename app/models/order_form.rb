339c0c4a6bf605161075fbd10bf9a9bfbb55a7ba
DICEDICE13 <orca.works2022@gmail.com>
2025-09-09 09:10:51 +0900

    prefecture バリデーション修正

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -8,15 +8,15 @@ class OrderForm
    with_options presence: true do
     
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
     validates :city
     validates :address_line
     validates :phone_number, format: { with: /\A\d{10,11}\z/,message: "is invalid"}
     validates :token
     validates :user_id
     validates :item_id
-    
   end
+
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 
 
   def save
